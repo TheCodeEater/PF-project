90389e90e05fa2f3f8dcc1fd8ae537ba5dfb7a95
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-04-28 15:26:12 +0200

    Added initial_config variable to represent the initial configuration. Now the application object is built with those options

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 namespace ps=particleSimulator;
 
 int main(){
-    ps::Application app{};
+    ps::options initial_config{2200,1230,"Simulatore di particelle"};
+    ps::Application app{initial_config};
 
     return app.loop();
 }
