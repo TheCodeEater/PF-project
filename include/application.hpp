75bc6a860377e4a1ae721d6a086bb62dd8ec54ec
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-04-28 15:29:03 +0200

    Formatted using clang-format

--- include/application.hpp
+++ include/application.hpp
@@ -4,38 +4,35 @@
 #include <SFML/Graphics.hpp>
 #include <string>
 
-namespace particleSimulator{
+namespace particleSimulator {
 
-//alias for sfml render window
-using W=sf::RenderWindow;
+// alias for sfml render window
+using W = sf::RenderWindow;
 
-    //simulation options
-    struct options{
-        int w_width{};
-        int w_height{};
-        std::string w_name{};
-    };
-
-class Application{
-
-    options optn_{};
-    W w_;//sfml render window
-
-    public:
-        Application(options const&);
-
-        ~Application()=default; //default destructir
-        //disable copy and move
-        Application(Application const&)=delete;
-        Application(Application&&)=delete;
-        Application& operator=(Application const&)=delete;
-        Application& operator=(Application&&)=delete;
-
-        int loop(); //run application loop
-
+// simulation options
+struct options {
+  int w_width{};
+  int w_height{};
+  std::string w_name{};
 };
 
-}
+class Application {
+  options optn_{};
+  W w_;  // sfml render window
 
+ public:
+  Application(options const&);
+
+  ~Application() = default;  // default destructir
+  // disable copy and move
+  Application(Application const&) = delete;
+  Application(Application&&) = delete;
+  Application& operator=(Application const&) = delete;
+  Application& operator=(Application&&) = delete;
+
+  int loop();  // run application loop
+};
+
+}  // namespace particleSimulator
 
 #endif
