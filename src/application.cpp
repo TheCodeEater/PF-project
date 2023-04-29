c2251570892fb87b16051302e778b4d7cab3a9ed
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-04-29 15:55:36 +0200

    Formatted using clang-format

--- src/application.cpp
+++ src/application.cpp
@@ -5,36 +5,35 @@ Application::Application(options const& opt)
     : optn_{opt},
       w_{sf::VideoMode(optn_.w_width, optn_.w_height), optn_.w_name} {}
 
-int Application::loop(){
-    // run the program as long as the window is open
-    while (w_.isOpen())
-    {
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event e;
-        while (w_.pollEvent(e))
-        {
-            //event handling
-            switch(e.type){
-              case sf::Event::Closed:
-                w_.close();
-                break;
+int Application::loop() {
+  // run the program as long as the window is open
+  while (w_.isOpen()) {
+    // check all the window's events that were triggered since the last
+    // iteration of the loop
+    sf::Event e;
+    while (w_.pollEvent(e)) {
+      // event handling
+      switch (e.type) {
+        case sf::Event::Closed:
+          w_.close();
+          break;
 
-              default:
-                //ignore
-                break;
-            }
-        }
-
-        //clear window. set black color
-        w_.clear(sf::Color::Black);
-
-        // draw everything here...
-        // window.draw(...);
-
-        // display
-        w_.display();
+        default:
+          // ignore
+          break;
+      }
     }
 
-    return 0;
+    // clear window. set black color
+    w_.clear(sf::Color::Black);
+
+    // draw everything here...
+    // window.draw(...);
+
+    // display
+    w_.display();
+  }
+
+  return 0;
 }
 }  // namespace particleSimulator
