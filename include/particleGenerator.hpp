1c7ab81f29cfbeb0066b3ed228629448ac6d3b20
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-04-29 18:34:41 +0200

    Added files for the particle generator

---
+++ include/particleGenerator.hpp
@@ -0,0 +1,8 @@
+#ifndef PARTICLE_GENERATOR_HPP
+#define PARTICLE_GENERATOR_HPP
+
+namespace particleSimulator{
+    
+}
+
+#endif
---
+++ src/particleGenerator.cpp
@@ -0,0 +1,5 @@
+#include "../include/particleGenerator.hpp"
+
+namespace particleGenerator{
+    
+}
