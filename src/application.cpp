d9cbe4bfc6cacd66d3cddf27421203e64bb20d7c
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-05-01 10:44:15 +0200

    Improved code readability

--- src/application.cpp
+++ src/application.cpp
@@ -8,12 +8,17 @@ Application::Application(options const& opt)
       x_{sf::Lines, 2},
       y_{sf::Lines, 2} {
   w_.setView(camera_);  // set the current view
+
+  //set x and y axis vertex position
   x_[0].position = {0, 0};
   x_[1].position = {optn_.w_width, 0};
-  x_[0].color = sf::Color::Green;
-  x_[1].color = sf::Color::Green;
+
   y_[1].position = {0, -optn_.w_height / 2.f};
   y_[0].position = {0, optn_.w_height / 2.f};
+
+  //set color
+  x_[0].color = sf::Color::Green;
+  x_[1].color = sf::Color::Green;
   y_[0].color = sf::Color::Green;
   y_[1].color = sf::Color::Green;
 }
