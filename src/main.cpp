676897f711cca82f0245e2960d7693deca75e690
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-05-09 11:44:45 +0200

    Aggiunti i valori delle coordinate delle rette che confinano le particelle all'inizializzazione della simulazione

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 namespace ps=particleSimulator;
 
 int main(){
-    ps::options initial_config{2200,1230,"Simulatore di particelle"};
+    ps::options initial_config{2200,1230,"Simulatore di particelle",400,300,700};
     ps::Application app{initial_config};
 
     return app.loop();
