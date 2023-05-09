4ec1e75b89f44107f8082855b6ae5094ccfaa2b7
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-05-09 11:48:17 +0200

    Implementazione base di un oggetto per gestire le impostazioni. Usata solo come placeholder, da sostituire con quella definitiva

--- include/io.hpp
+++ include/io.hpp
@@ -1,9 +1,15 @@
 #ifndef IO_HPP
 #define IO_HPP
 
-
+#include "application.hpp"
 namespace particleSimulator{
 
+class config{
+
+    public:
+     config()=default;
+     options getOptions() const;
+};
 }
 
 
