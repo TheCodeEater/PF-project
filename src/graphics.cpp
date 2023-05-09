23d07f1ab981fb94081deeedf50809126404a771
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-05-09 13:04:18 +0200

    Generazione dei punti ora eseguita con algoritmi. Il vettore direttore della retta e' ora immagazzinato come suo membro

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -2,6 +2,7 @@
 
 #include <cmath>
 #include <iostream>
+#include <algorithm>
 namespace particleSimulator {
 
 sf::Vector2f operator*(float t, sf::Vector2f v){
@@ -9,19 +10,16 @@ sf::Vector2f operator*(float t, sf::Vector2f v){
 }
 
 dottedLine::dottedLine(sf::Vector2f p0, sf::Vector2f p1)
-    : first_{p0}, last_{p1}, vertices_{sf::Points, 2} {
-        //crea i punti intermedi
-        //usa uno step
-        const int step{10};
-        const sf::Vector2f v{p1-p0}; //vettore direttore della retta
-        auto r=[p0,v](int t){
-            return 0.1*(p0+t*v);
-        };
-        for(int i{};i<20;++i){
-            sf::Vertex vert{r(i)};
-            vert.color=sf::Color::Red;
-            vertices_.append(vert);
-        }
+    : first_{p0}, last_{p1}, direction_{last_-first_}, vertices_{sf::Points, 2} {
+        //genera N punti della linea e inseriscili mediante back inserter
+        int i{};
+        std::generate_n(std::back_inserter(*this),20,[this,&i](){
+            //calcola la posizione
+            sf::Vertex vertex{0.1*(first_+i*direction_)};
+            ++i;
+
+            return vertex;
+        });
     }
 
 void dottedLine::draw(sf::RenderWindow& w_) const { w_.draw(vertices_); }
@@ -32,6 +30,10 @@ std::pair<sf::Vector2f,sf::Vector2f> dottedLine::getExtremes() const{
 
 sf::VertexArray const& dottedLine::getVertexArray() const{
     return vertices_;
+}
+
+void dottedLine::push_back(value_type vertex){
+    vertices_.append(vertex);
 }
 
 }  // namespace particleSimulator
