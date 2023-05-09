b057eba9c43e6cb591dfe91a4f8fbdc82e0da2ac
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-05-09 17:41:48 +0200

    Aggiunto parametro di colore per i punti della linea. Il colore è attribuito durante la creazione dei vertici Aggiunto cast statico nel calcolo del numero di punti da disegnare

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -9,7 +9,7 @@ sf::Vector2f operator*(float t, sf::Vector2f v){
     return {t*v.x,t*v.y};
 }
 
-dottedLine::dottedLine(sf::Vector2f p0, sf::Vector2f p1)
+dottedLine::dottedLine(sf::Vector2f p0, sf::Vector2f p1, sf::Color c)
     : first_{p0}, last_{p1}, direction_{last_-first_}, vertices_{sf::Points, 2} {
         //genera punti con un dato step di incremento del parametro finché non superi il punto finale
         const float step{0.2f};
@@ -20,12 +20,14 @@ dottedLine::dottedLine(sf::Vector2f p0, sf::Vector2f p1)
         //norma del segmento tra punto zero e primo punto
         const double l_1=std::sqrt(q.x*q.x+q.y*q.y);
         //calcola il numero di punti per rappresentare la retta con il dato step al parametro e arrotonda
-        const int N{std::round(l/l_1)};
+        const int N{static_cast<int>(std::round(l/l_1))};
         //genera N punti della linea e inseriscili mediante back inserter
         float t{0.f};
-        std::generate_n(std::back_inserter(*this),N,[this,&t,&step](){
+        std::generate_n(std::back_inserter(*this),N,[this,&t,&step,&c](){
             //calcola la posizione
             sf::Vertex vertex{first_+t*direction_};
+            vertex.color=c;
+
             t+=step;
 
             return vertex;
