bec40727dd8d3c240751e819015d46368dde6cfb
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-05-09 12:06:00 +0200

    Aggiunto oggetto per rappresentare graficamente la traiettoria delle particelle

--- include/graphics.hpp
+++ include/graphics.hpp
@@ -1,7 +1,25 @@
 #ifndef GRAPHICS_HPP
 #define GRAPHICS_HPP
 
+#include <SFML/Graphics.hpp>
+
 namespace particleSimulator{
+
+class dottedLine{
+    sf::Vector2f first_{};
+    sf::Vector2f last_{};
+
+    sf::VertexArray vertices_{};
+
+    public:
+        dottedLine(sf::Vector2f p0,sf::Vector2f p1);
+
+        void setPoints(sf::Vector2f,sf::Vector2f);
+        void setPoint1(sf::Vector2f);
+        void setPoint2(sf::Vector2f);
+
+        void draw(sf::RenderWindow&) const;
+};
 
 }
 
