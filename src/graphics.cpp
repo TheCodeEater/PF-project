9b5ad95a8fd84558b079d1ab74704778e7a00b81
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-05-09 12:06:36 +0200

    Implementazione della funzione disegno per dottedLine e base per il costruttore

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -1,5 +1,9 @@
 #include "../include/graphics.hpp"
 
 namespace particleSimulator{
-    
+    dottedLine::dottedLine(sf::Vector2f p0,sf::Vector2f p1): first_{p0}, last_{p0}, vertices_{sf::Points,2} {}
+
+    void dottedLine::draw(sf::RenderWindow& w_) const{
+        w_.draw(vertices_);
+    }
 }
