80c116a15d55d8994eac9452cf7256832fcc69fd
Eowyn <stella.baldrati@studio.unibo.it>
2023-05-10 13:25:24 +0200

    Aggiunto traiettoria (.cpp e .hpp) creata classe traiettoria e particella --> con Eigen :(

--- include/traiettoria.hpp
+++ include/traiettoria.hpp
@@ -1,8 +1,26 @@
 #ifndef TRAIETTORIA_HPP
 #define TRAIETTORIA_HPP
 
+#include <SFML/Graphics.hpp>
+#include "../Eigen/Dense"
+#include <cmath>
+
 namespace particleSimulator{
+    struct particle{
+        Eigen::Vector2f pos{}; //pos sta per position
+        double theta{};
+    };
     
+    class path {
+        sf::VertexArray const& borderup_{};
+        sf::VertexArray const& borderdown_{};
+        
+        
+        public:
+        path(sf::VertexArray const&, sf::VertexArray const&);
+        Eigen::Vector2f operator()(particle);
+    
+    };
 }
 
 #endif
--- src/traiettoria.cpp
+++ src/traiettoria.cpp
@@ -1,5 +1,13 @@
 #include "../include/traiettoria.hpp"
+#include <cmath>
 
 namespace particleSimulator{
+path::path(sf::VertexArray const& bup, sf::VertexArray const& bdown): borderup_{bup}, borderdown_{bdown}{}
+
+Eigen::Vector2f path::operator()(particle p){
+    Eigen::Vector2f dir{std::cos(p.theta), std::sin(p.theta)};
+    }
+
+
 
 }
