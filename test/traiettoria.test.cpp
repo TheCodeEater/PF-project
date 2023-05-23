7a40c550375bdaa1e27f09592af560c0067a4740
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-05-23 20:45:57 +0200

    Aumentata la precisione del calcolo del pi greco. Prima non funzionava poiché effettuavamo una divisione intera per ottenere un numero float

--- test/traiettoria.test.cpp
+++ test/traiettoria.test.cpp
@@ -4,11 +4,11 @@
 #include "../include/traiettoria.hpp"
 
 TEST_CASE("test traiettoria con Eigen"){
-    particleSimulator::particle p{{0,0}, 22/7/4};
-    Line borderup{Line::Through({0,10},{100,5})};
+    particleSimulator::particle p{{0,0}, std::atan(1)}; //particella con angolo di 45 gradi nell'origine
+    Line borderup{Line::Through({0,10},{100,5})}; //linea bordo di esempio
     //Line borderdown;
-    particleSimulator::path pluto{borderup, Line::Through({0,0},{0,0})};
+    particleSimulator::path pluto{borderup, Line::Through({0,0},{0,0})}; //oggetto di calcolo della traiettoria
     Eigen::Vector2f intersection = pluto(p);
-    CHECK(intersection.x()==doctest::Approx(9.5));
+    CHECK(intersection.x()==doctest::Approx(9.5238));
 
 }
