008ba0a758bf89be724407c85053eb9c97639033
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-05-24 21:34:15 +0200

    Formattato

--- src/traiettoria.cpp
+++ src/traiettoria.cpp
@@ -1,21 +1,22 @@
 #include "../include/traiettoria.hpp"
+
 #include <cmath>
 
-namespace particleSimulator{
-path::path(Line const& bup, Line const& bdown): borderup_{bup}, borderdown_{bdown}{}
+namespace particleSimulator {
+path::path(Line const& bup, Line const& bdown)
+    : borderup_{bup}, borderdown_{bdown} {}
 
-Eigen::Vector2f path::operator()(particle const& p) const{
-    const Eigen::Vector2f dir{std::cos(p.theta), std::sin(p.theta)}; //direzione particella
-    //piccolo test
-    assert(dir.norm()-1<1e-5);
-    const Eigen::ParametrizedLine<float,2> trajectory{p.pos,dir}; //retta della direzione della particella
-    //tentativi falliti
-    //Eigen::Matrix2f sist1{dir, borderup_};
-    //Eigen::Matrix2f sist2{dir, borderdown_};
-    const Eigen::Vector2f intersection = trajectory.intersectionPoint(borderup_); //intersezione traiettoria con i bordi
-    return intersection;
-    }
+Eigen::Vector2f path::operator()(particle const& p) const {
+  const Eigen::Vector2f dir{std::cos(p.theta),
+                            std::sin(p.theta)};  // direzione particella
+  // piccolo test
+  assert(dir.norm() - 1 < 1e-5);
+  const Eigen::ParametrizedLine<float, 2> trajectory{
+      p.pos, dir};  // retta della direzione della particella
 
-
-
+  const Eigen::Vector2f intersection = trajectory.intersectionPoint(
+      borderup_);  // intersezione traiettoria con i bordi
+  return intersection;
 }
+
+}  // namespace particleSimulator
