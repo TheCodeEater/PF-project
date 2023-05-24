d689b21a443fd9777eff8aa3659090c05d389f40
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-05-24 21:34:52 +0200

    Formattato

--- include/traiettoria.hpp
+++ include/traiettoria.hpp
@@ -2,26 +2,28 @@
 #define TRAIETTORIA_HPP
 
 #include <SFML/Graphics.hpp>
-#include "../Eigen/Dense"
 #include <cmath>
 
-using Line=Eigen::Hyperplane<float, 2>;
-namespace particleSimulator{
-    struct particle{
-        Eigen::Vector2f pos{}; //pos sta per position
-        double theta{};  //pos e theta da input (distribuzione)
-    };
-    
-    class path {  //contiene i bordi del biliardo
-        Line borderup_{};
-        Line borderdown_{};
-        
-        
-        public:
-        path(Line const&, Line const&);
-        Eigen::Vector2f operator()(particle);  // operatore () per calcolare la traiettoria
-    
-    };
-}
+#include "../Eigen/Dense"
+
+using Line = Eigen::Hyperplane<float, 2>;
+namespace particleSimulator {
+struct particle {
+  Eigen::Vector2f pos{};  // pos sta per position
+  double theta{};         // pos e theta da input (distribuzione)
+};
+
+class path {  // contiene i bordi del biliardo
+  Line borderup_{};
+  Line borderdown_{};
+
+ public:
+  path(Line const&, Line const&);
+  Eigen::Vector2f operator()(particle const&)
+      const;  // operatore () per calcolare la traiettoria riflessa
+
+  Line getNormal() const;
+};
+}  // namespace particleSimulator
 
 #endif
