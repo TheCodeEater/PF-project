f392cb72b190726980b65cde2d6e1c04c0baa718
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-05-25 18:06:22 +0200

    Test divisi in sottocasi (base)

--- test/traiettoria.test.cpp
+++ test/traiettoria.test.cpp
@@ -4,11 +4,14 @@
 #include "../include/traiettoria.hpp"
 
 TEST_CASE("test traiettoria con Eigen"){
-    particleSimulator::particle p{{0,0}, std::atan(1)}; //particella con angolo di 45 gradi nell'origine
-    Line borderup{Line::Through({0,10},{100,5})}; //linea bordo di esempio
-    //Line borderdown;
-    particleSimulator::path pluto{borderup, Line::Through({0,-10},{100,-5})}; //oggetto di calcolo della traiettoria
-    Eigen::Vector2f intersection = pluto(p);
-    CHECK(intersection.x()==doctest::Approx(9.5238));
+
+    particleSimulator::path biliardo{Line::Through({0,10},{100,5}), Line::Through({0,-10},{100,-5})}; //oggetto di calcolo della traiettoria
+
+    SUBCASE("Collisioni con il bordo superiore"){
+        particleSimulator::particle p{{0,0}, std::atan(1)}; //particella con angolo di 45 gradi nell'origine
+        const Eigen::Vector2f intersection = biliardo(p);
+        
+        CHECK(intersection.x()==doctest::Approx(9.5238));
+    }
 
 }
