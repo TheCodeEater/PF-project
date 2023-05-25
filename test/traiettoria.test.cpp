df7055d5b464dfce445bdf025cfc9cdcde3cbf1b
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-05-25 15:58:04 +0200

    Aggiunto bordo inferiore

--- test/traiettoria.test.cpp
+++ test/traiettoria.test.cpp
@@ -7,7 +7,7 @@ TEST_CASE("test traiettoria con Eigen"){
     particleSimulator::particle p{{0,0}, std::atan(1)}; //particella con angolo di 45 gradi nell'origine
     Line borderup{Line::Through({0,10},{100,5})}; //linea bordo di esempio
     //Line borderdown;
-    particleSimulator::path pluto{borderup, Line::Through({0,0},{0,0})}; //oggetto di calcolo della traiettoria
+    particleSimulator::path pluto{borderup, Line::Through({0,-10},{100,-5})}; //oggetto di calcolo della traiettoria
     Eigen::Vector2f intersection = pluto(p);
     CHECK(intersection.x()==doctest::Approx(9.5238));
 
