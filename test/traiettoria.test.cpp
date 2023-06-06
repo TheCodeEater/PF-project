820178b1704f6c5e0954cb140b93c53807d7483c
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-06-06 13:51:01 +0200

    Creati test di prova per 3 particelle. Da riorganizzare (alcuni falliscono perchè non ho ancora implementato un caso)

--- test/traiettoria.test.cpp
+++ test/traiettoria.test.cpp
@@ -2,10 +2,14 @@
 
 #include "doctest.h"
 #include "../include/traiettoria.hpp"
+#include <iostream>
 
 TEST_CASE("test traiettoria con Eigen"){
 
-    particleSimulator::path biliardo{Line::Through({0,10},{100,5}), Line::Through({0,-10},{100,-5})}; //oggetto di calcolo della traiettoria
+    const int r1=10;
+    const int r2=5;
+    const int l=100;
+    particleSimulator::path biliardo{r1,r2,l}; //oggetto di calcolo della traiettoria
 
     SUBCASE("Collisioni con il bordo superiore"){
         particleSimulator::particle p{{0,0}, std::atan(1)}; //particella con angolo di 45 gradi nell'origine
@@ -13,5 +17,32 @@ TEST_CASE("test traiettoria con Eigen"){
         
         CHECK(intersection.x()==doctest::Approx(9.5238));
     }
-
 }
+TEST_CASE("Test dell'orientazione del vettore"){
+        const int r1=10;
+        const int r2=5;
+        const int l=100;
+        particleSimulator::path biliardo{r1,r2,l};
+
+        SUBCASE("Particella 1"){
+            const particleSimulator::particle p{{0,0}, particleSimulator::path::arctan(-1,-1)};
+            const Eigen::Vector2f intersection = biliardo(p);
+
+            CHECK(biliardo.getHitDirection(p.theta)==particleSimulator::vecOrientation::Down);
+        }
+        SUBCASE("Particella 2"){
+            const particleSimulator::particle p1{{0,0}, std::atan(0)};
+            const Eigen::Vector2f intsect=biliardo(p1);
+            CHECK(biliardo.getHitDirection(p1.theta)==particleSimulator::vecOrientation::Horizontal);
+        }
+        SUBCASE("Particella 3"){
+            const particleSimulator::particle p2{{0,5}, particleSimulator::path::arctan(0,-1)};
+            const Eigen::Vector2f intsect=biliardo(p2);
+
+            std::cout<<intsect<<"\n";
+
+            CHECK(biliardo.getHitDirection(p2.theta)==particleSimulator::vecOrientation::Horizontal);
+            CHECK(biliardo.getLocationType(intsect)==particleSimulator::posTypes::BackHit);
+        }
+        
+    }
