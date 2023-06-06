a870de317f08dbf6663104181b0d683bed4960e1
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-06-06 14:52:13 +0200

    Riorganizzato il codice in subcase
    Test riscritti in modo più ordinato, al momento tutti passati
    
    NOTA: la particella due colpisce il bordo ma la funzione restituisce inside, perché giustamente è inside
    
    La chiamata a getLocationType effettuata dentro l'operator() che aggiusta il punto di intersezione se questo ha x negative restituisce backHit, ma poi viene aggiustato
    Non si può spostare il caso del bordo in backhit (mettendo inside solo su x>0) perchè poi scatta l'assert che vuole che la particella sia dentrRiorganizzato il codice in subcase
    
    Test riscritti in modo più ordinato, al momento tutti passati
    
    NOTA: la particella due colpisce il bordo ma la funzione restituisce inside, perché giustamente è inside
    
    La chiamata a getLocationType effettuata dentro l'operator() che aggiusta il punto di intersezione se questo ha x negative restituisce backHit, ma poi viene aggiustato
    Non si può spostare il caso del bordo in backhit (mettendo inside solo su x>0) perchè poi scatta l'assert che vuole che la particella sia dentroo

--- test/traiettoria.test.cpp
+++ test/traiettoria.test.cpp
@@ -4,45 +4,43 @@
 #include "../include/traiettoria.hpp"
 #include <iostream>
 
-TEST_CASE("test traiettoria con Eigen"){
+using pT=particleSimulator::posTypes;
 
+TEST_CASE("Test del calcolo delle traiettorie"){
+    //dati del biliardo di esempio
     const int r1=10;
     const int r2=5;
     const int l=100;
+
     particleSimulator::path biliardo{r1,r2,l}; //oggetto di calcolo della traiettoria
 
-    SUBCASE("Collisioni con il bordo superiore"){
-        particleSimulator::particle p{{0,0}, std::atan(1)}; //particella con angolo di 45 gradi nell'origine
-        const Eigen::Vector2f intersection = biliardo(p);
-        
-        CHECK(intersection.x()==doctest::Approx(9.5238));
+    //particelle
+    const particleSimulator::particle p0{{0,0}, std::atan(1)};
+    const particleSimulator::particle p1{{0,0}, particleSimulator::path::arctan(-1,-1)};
+    const particleSimulator::particle p2{{0,0}, std::atan(0)};
+    const particleSimulator::particle p3{{0,5}, particleSimulator::path::arctan(0,-1)};
+
+    //Calcolo orientazione particella
+    SUBCASE("Test calcolo orientazione traiettoria"){
+        CHECK(biliardo.getHitDirection(p0.theta)==particleSimulator::vecOrientation::Up);
+        CHECK(biliardo.getHitDirection(p1.theta)==particleSimulator::vecOrientation::Down);
+        CHECK(biliardo.getHitDirection(p2.theta)==particleSimulator::vecOrientation::HorizontalRight);
+        CHECK(biliardo.getHitDirection(p3.theta)==particleSimulator::vecOrientation::HorizontalLeft);
+
     }
+    
+    //calcolo intersezioni
+    const Eigen::Vector2f int0=biliardo(p0);
+    const Eigen::Vector2f int1=biliardo(p1);
+    const Eigen::Vector2f int2=biliardo(p2);
+    const Eigen::Vector2f int3=biliardo(p3);
+
+    SUBCASE("Test collisioni dopo il lancio"){
+        CHECK(biliardo.getLocationType(int0)==pT::Inside);
+        CHECK(biliardo.getLocationType(int1)==pT::Inside); //NOTA: sarebbe un colpo nel bordo, ma resta comunque dentro
+        CHECK(biliardo.getLocationType(int2)==pT::Escaped);
+        CHECK(biliardo.getLocationType(int3)==pT::Inside);
+
+    }
+
 }
-TEST_CASE("Test dell'orientazione del vettore"){
-        const int r1=10;
-        const int r2=5;
-        const int l=100;
-        particleSimulator::path biliardo{r1,r2,l};
-
-        SUBCASE("Particella 1"){
-            const particleSimulator::particle p{{0,0}, particleSimulator::path::arctan(-1,-1)};
-            const Eigen::Vector2f intersection = biliardo(p);
-
-            CHECK(biliardo.getHitDirection(p.theta)==particleSimulator::vecOrientation::Down);
-        }
-        SUBCASE("Particella 2"){
-            const particleSimulator::particle p1{{0,0}, std::atan(0)};
-            const Eigen::Vector2f intsect=biliardo(p1);
-            CHECK(biliardo.getHitDirection(p1.theta)==particleSimulator::vecOrientation::Horizontal);
-        }
-        SUBCASE("Particella 3"){
-            const particleSimulator::particle p2{{0,5}, particleSimulator::path::arctan(0,-1)};
-            const Eigen::Vector2f intsect=biliardo(p2);
-
-            std::cout<<intsect<<"\n";
-
-            CHECK(biliardo.getHitDirection(p2.theta)==particleSimulator::vecOrientation::Horizontal);
-            CHECK(biliardo.getLocationType(intsect)==particleSimulator::posTypes::BackHit);
-        }
-        
-    }
