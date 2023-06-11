ddc70cec869875bb36f2b5f5cf2d523b9b567e10
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-06-11 11:42:14 +0200

    Scritto test per la generazione dei numeri in range

--- test/generator.test.cpp
+++ test/generator.test.cpp
@@ -3,15 +3,19 @@
 #include "doctest.h"
 
 #include "../include/particleGenerator.hpp"
+#include "../include/traiettoria.hpp"
+
 
 TEST_CASE("Test della generazione in range dei numeri"){
     const float r1_{400}; //parametro r1
 
-    const particleSimulator::randSimulator rs{0.f,1.f,0.f,pi/4,r1_,10};
+    particleSimulator::randSimulator rs{0.f,90.f,0.f,pi/4,r1_};
 
-    auto pos_gen=rs.getPosGenerator();
-    auto ang_gen=rs.getAngleGenerator();
+    for(int i{};i<1e6;++i){
+        particleSimulator::particle p{rs()};
 
-    CHECK(pos_gen.max()<=r1_);
-    CHECK(pos_gen.min()>=-r1_);
+        CHECK(p.pos.y()<=r1_);
+        CHECK(p.pos.y()>=-r1_);
+    }
+
 }
