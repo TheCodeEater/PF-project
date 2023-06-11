5884462bca0ce1d2f1ee95927edbeeedd9dc2c14
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-06-11 10:50:22 +0200

    Creato oggetto per la generazione casuale delle particelle

--- include/particleGenerator.hpp
+++ include/particleGenerator.hpp
@@ -1,6 +1,19 @@
 #ifndef PARTICLE_GENERATOR_HPP
 #define PARTICLE_GENERATOR_HPP
 
-namespace particleSimulator {}
+#include <random>
+namespace particleSimulator {
+
+class randSimulator{ //classe per la simulazione con i numeri casuali
+    std::default_random_engine engine_;
+
+    std::normal_distribution<float> pos_dist_;
+    std::normal_distribution<float> angle_dist_;
+
+    public:
+        randSimulator(float pos_mean, float pos_sigma, float angle_mean, float angle_sigma);
+};
+
+}
 
 #endif
--- src/particleGenerator.cpp
+++ src/particleGenerator.cpp
@@ -1,3 +1,8 @@
 #include "../include/particleGenerator.hpp"
 
-namespace particleGenerator {}
+namespace particleSimulator {
+
+randSimulator::randSimulator(float pos_mean, float pos_sigma, float angle_mean, float angle_sigma): //constructor
+    engine_{std::random_device{}()}, pos_dist_{pos_mean,pos_sigma}, angle_dist_{angle_mean,angle_sigma} {}
+
+}
