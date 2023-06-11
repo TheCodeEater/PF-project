f0735027b75e5017b693e78805ddc50cb5512269
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-06-11 16:33:10 +0200

    Creato generatore di particelle. Il test verifica (al momento) se scattano assert

--- test/generator.test.cpp
+++ test/generator.test.cpp
@@ -5,17 +5,31 @@
 #include "../include/particleGenerator.hpp"
 #include "../include/traiettoria.hpp"
 
+#include <fstream>
 
 TEST_CASE("Test della generazione in range dei numeri"){
-    const float r1_{400}; //parametro r1
+    const float r1{400}; //parametro r1
+    const float r2{200};
+    const float l{700};
 
-    particleSimulator::randSimulator rs{0.f,90.f,0.f,pi/4,r1_};
+    particleSimulator::path biliardo{r1,r2,l};
 
-    for(int i{};i<1e6;++i){
-        particleSimulator::particle p{rs()};
+    particleSimulator::randSimulator rs{0.f,80.f,0.f,pi/4,biliardo};
 
-        CHECK(p.pos.y()<=r1_);
-        CHECK(p.pos.y()>=-r1_);
+    /*for(int i{};i<1e3;++i){
+        particleSimulator::particle p{rs.getParticle()};
+
+        CHECK(p.pos.y()<=r1);
+        CHECK(p.pos.y()>=-r1);
+    }*/
+
+    //TEST
+    std::vector<particleSimulator::particle> exit=rs.run(1e5,200);
+
+    std::ofstream f{"output.txt"};
+
+    for(auto const& v:exit){
+        f<<v.theta<<"\n";
     }
 
 }
