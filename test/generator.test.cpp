9db5df1c7a0edefab41b4c1150de5cf648e79ffc
TheCodeEater <35703721+TheCodeEater@users.noreply.github.com>
2023-06-14 10:41:50 +0200

    Adattati test alla nuova struttura del programma. Aggiunto controllo eccezioni e determinazione del seed incriminato a generator test

--- test/generator.test.cpp
+++ test/generator.test.cpp
@@ -11,36 +11,26 @@ TEST_CASE("Test della generazione in range dei numeri"){
     const float r1{400}; //parametro r1
     const float r2{200};
     const float l{700};
+    const unsigned seed{std::random_device{}()};
 
     particleSimulator::path biliardo{r1,r2,l};
 
-    particleSimulator::randSimulator rs{{r1,r2,l,0.f,80.f,0.f,pi/8,100}};
+    particleSimulator::randSimulator rs{{r1,r2,l,0.f,80.f,0.f,pi/8,seed}};
 
-    /*for(int i{};i<1e3;++i){
-        particleSimulator::particle p{rs.getParticle()};
 
-        CHECK(p.pos.y()<=r1);
-        CHECK(p.pos.y()>=-r1);
-    }*/
+    std::vector<particleSimulator::exit_point> exit{};
 
-    //TEST
-    /*for(int i{};i<1106;++i){
-        particleSimulator::particle p{rs.getParticle()};
+    try{
+        exit=rs.run(100000,200);
+        std::ofstream f{"output.txt"};
+        for(auto const& v:exit){
+            f<<v.theta<<"\n";
+        }
+    }catch(std::exception const& e){
+        std::cout<<e.what()<<"\n";
+        std::cout<<"Seed incriminato: "<<seed<<"\n";
+        throw;
     }
 
-    particleSimulator::particle bad_p=rs.getParticle();
-
-    std::cout<<bad_p.pos<<"\n"<<bad_p.theta<<"\n";*/
-
-    std::vector<particleSimulator::exit_point> exit=rs.run(233,200);
-
-    auto p=rs.getParticle();
-    std::cout<<p.pos<<"\n"<<p.theta<<"\n";
-
-    std::ofstream f{"output.txt"};
-
-    for(auto const& v:exit){
-        f<<v.theta<<"\n";
-    }
 
 }
